076822b115974879b66041275347a6922fadaf7a
Euan Barrett <ergoeuan@gmail.com>
2020-04-18 23:42:10 +0100

    fix2electricboogaloo

--- CRUDTODONewdb/project/models/task.js
+++ CRUDTODONewdb/project/models/task.js
@@ -4,17 +4,17 @@ const Schema = mongoose.Schema;
 
 //data inputted into the database, coursework/milestones section, validation included
 const taskSchema = new Schema({
-	task: {
-		type: String,
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	},
-	addedAt: {
-		type: Date,
-		default: Date.now()
-	},
+    task: {
+        type: String,
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    addedAt: {
+        type: Date,
+        default: Date.now()
+    },
 });
 
 const Task = module.exports = mongoose.model('Task', taskSchema);
